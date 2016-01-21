a9edb45a815fbfd9c640a1de2012fd2c336d56c4
Veierovioum <mcattar@gmail.com>
2016-01-21 23:52:43 +0200

    lesson11

---
+++ lesson11/src/main/java/com/example/veierovioum/lesson11/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.veierovioum.lesson11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void onClick(View v){
+        v.setSelected(!v.isSelected());
+    }
+}
