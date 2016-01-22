39e19365ebab8bd17c299c3f810d8b8a9237ef0e
Veierovioum <mcattar@gmail.com>
2016-01-22 22:18:54 +0200

    lesson11-3 multi screens

--- lesson11/src/main/java/com/example/veierovioum/lesson11/ThirdActivity.java
+++ lesson11/src/main/java/com/example/veierovioum/lesson11/ThirdActivity.java
@@ -2,6 +2,8 @@ package com.example.veierovioum.lesson11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        View btn2=findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ThirdActivity.this, ""+v.getWidth(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
