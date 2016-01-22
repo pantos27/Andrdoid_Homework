8eae677e8e8bf766ed84e1694b9d9e0c4e7bce3d
Veierovioum <mcattar@gmail.com>
2016-01-22 22:20:38 +0200

    השלמות

---
+++ lesson10-1/src/main/java/com/example/veierovioum/lesson10_1/PlaneActivity.java
@@ -0,0 +1,17 @@
+package com.example.veierovioum.lesson10_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plane_activity);
+
+
+
+
+    }
+}
